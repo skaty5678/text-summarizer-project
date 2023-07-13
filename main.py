26cc7c4f58fad12f08f532e60849aa463cf22941
skaty5678 <skaty5678@gmail.com>
2023-07-14 01:05:43 +0530

    added ipynb file

---
+++ main.py
@@ -0,0 +1,4 @@
+from text_summarizer.logging import logger
+
+logger.info('welcome to our custom logging')
+
---
+++ src/text_summarizer/utils/common.py
@@ -0,0 +1,61 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from text_summarizer.logging import logger
+from ensure import ensure_annotations
+from pathlib import Path
+from typing import Any
+from box import ConfigBox
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox Type
+    """
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs js to be created. Default to False.
+
+    """
+    for path in path_to_directories:
+        if verbose:
+            logger.info(f"created directory at {path}")
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """create list of directories
+
+    Args:
+        path: path of the file
+    Returns:
+        str: size in kb
+    """
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    return f"~ {size_in_kb} KB"
